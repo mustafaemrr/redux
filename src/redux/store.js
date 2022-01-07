b66eb7f0e8309585bb87cffb8ce8b089919ab62c
Mustafa Emir <info@mustafaemir.com>
2022-01-07 22:48:14 +0300

    create todos slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from './counter/counterSlice';
+import todosSlice from './todos/todosSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
+    todos: todosSlice
   },
 });
