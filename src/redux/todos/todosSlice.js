2b82e17ec8b3001c79f5b0f530eaa5ac9feeb76f
Mustafa Emir <info@mustafaemir.com>
2022-01-08 21:47:17 +0300

    create addTodo action

--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -3,20 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 export const todosSlice = createSlice({
   name:'todos',
   initialState: {
-    items: [
-      {
-        id: 1,
-        title: 'Learn React',
-        completed: true
-      },
-      {
-        id: 2,
-        title: 'Read a book',
-        completed: true
-      },
-    ],
+    items: [],
   },
-  reducers: {}
+  reducers: {
+    addTodo: (state, action) => {
+      state.items.push(action.payload);
+    }
+  }
 });
 
 export default todosSlice.reducer;
