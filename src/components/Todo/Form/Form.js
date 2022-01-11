77e9aaf47f9905df340eed840107ec78c98c0823
Mustafa Emir <info@mustafaemir.com>
2022-01-11 23:38:55 +0300

    add error catch

--- src/components/Todo/Form/Form.js
+++ src/components/Todo/Form/Form.js
@@ -4,9 +4,10 @@ import { addTodosAsync } from '../../../redux/todos/todosSlice';
 
 function Form() {
   const [title, setTitle] = useState('');
-  const newTodoIsLoding = useSelector((state) => state.todos.addNewTodoIsLoding);
-
   const dispatch = useDispatch();
+  const newTodoIsLoding = useSelector((state) => state.todos.addNewTodoIsLoding);
+  const error = useSelector((state) => state.todos.addNewTodoError);
+
 
   const handleSubmit = async (e) => {
     if (!title) return;
@@ -19,13 +20,20 @@ function Form() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input disabled={newTodoIsLoding} className="new-todo" placeholder="What needs to be done?" autoFocus value={title} onChange={(e) => setTitle(e.target.value)} />
+    <>
+      {
+        error && (
+          <div>{error}</div>
+        )
+      }
+      <form onSubmit={handleSubmit}>
+        <input disabled={newTodoIsLoding} className="new-todo" placeholder="What needs to be done?" autoFocus value={title} onChange={(e) => setTitle(e.target.value)} />
 
-      { newTodoIsLoding && (
-        <img src="https://flevix.com/wp-content/uploads/2020/01/Circle-Loading.svg" alt="" />
-      )}
-    </form>
+        { newTodoIsLoding && (
+          <img src="https://flevix.com/wp-content/uploads/2020/01/Circle-Loading.svg" alt="" />
+        )}
+      </form>
+    </>
   )
 }
 
